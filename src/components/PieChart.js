abef5ec2618ed0e173a98101d72f3070c30b9a55
Andreas Savvides <asavvides@twitter.com>
2015-10-18 20:18:55 +0100

    [#9] Add logic for adding legend to pie/donut charts

--- src/components/PieChart.js
+++ src/components/PieChart.js
@@ -24,9 +24,59 @@ export default class PieChart extends BaseChart {
             .text(`${d.data.key} (${d.data.value})`);
     }
 
+    addLegend() {
+        this.legend = this.svg.selectAll(".d3act-legend")
+            .data(this.color.domain())
+            .enter()
+            .append("g")
+            .attr("class", "d3act-legend")
+            .attr("transform", (d, i) => {
+                const height = this.legendRectSize + this.legendSpacing;
+                const offset = 20;
+                const horz = -2 * this.legendRectSize;
+                const vert = (this.props.height / 2) + (i * height) + offset;
+
+                return "translate(" + horz + "," + vert + ")";
+            });
+
+        this.legend.append("rect")
+            .attr("width", this.legendRectSize)
+            .attr("height", this.legendRectSize)
+            .style("fill", this.color)
+            .style("stroke", this.color);
+
+        this.legend.append("text")
+            .attr("x", this.legendRectSize + this.legendSpacing)
+            .attr("y", this.legendRectSize - this.legendSpacing)
+            .text(d => {
+                const name = Object.keys(this.data)[d];
+                const value = this.data[name];
+
+                return `${name} (${value})`;
+            });
+    }
+
+    getHeight(numberOfItems) {
+        if (this.showLegend) {
+            const legendRectExtraHeight = (numberOfItems * this.legendRectSize);
+            const legendSpacingExtraHeight = (numberOfItems * this.legendSpacing);
+
+            return (this.props.height * 1.5) + legendSpacingExtraHeight + legendRectExtraHeight;
+        }
+
+        return this.props.height;
+    }
+
     create(data) {
+        this.legendRectSize = 18;
+        this.legendSpacing = 4;
+        this.data = data;
+
+        const numberOfItems = Object.keys(data).length;
+
         const width = this.props.width;
         const height = this.props.height;
+        const svgHeight = this.getHeight(numberOfItems);
         const radius = Math.min(width, height) / 2;
         const halfWidth = width / 2;
         const halfHeight = height / 2;
@@ -41,7 +91,7 @@ export default class PieChart extends BaseChart {
 
         this.svg = d3.select(this.el).append("svg")
             .attr("width", width)
-            .attr("height", height)
+            .attr("height", svgHeight)
             .append("g")
                 .attr("transform", `translate(${halfWidth}, ${halfHeight})`);
 
@@ -64,6 +114,10 @@ export default class PieChart extends BaseChart {
 
         if (this.showTooltips) {
             this.addTooltips();
+        }
+
+        if (this.showLegend) {
+            this.addLegend();
         }
     }
 
